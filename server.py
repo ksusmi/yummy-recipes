82497095fa23b251534e1c5eba55185a7f1f021c
ksusmi <susmi.k@gmail.com>
2021-05-07 16:13:10 +0000

    clean up the code server.py

--- server.py
+++ server.py
@@ -23,20 +23,11 @@ def homepage():
     return render_template('homepage.html', trivia=trivia, recipe=recipe, recipe1=recipe1)
 
 
-@app.route('/search')
-def search():
-    """get the search value from homepage and store in session."""
-    search = request.args.get('search')
-    session['search'] = search
-
-    return redirect('/search/results')
-
-
 @app.route('/search/results')
 def search_result():
     """View search form."""
-
-    s = session['search']
+    s = request.args.get('search')
+   
     response_recipe_complex_json = rapidapi.get_recipe_complex_search(
         query=s, number=21)
     res = response_recipe_complex_json["results"]
@@ -130,44 +121,41 @@ def get_recipe_details():
             show_fav_link = False
             recipe_details['review_notes'] = check_rating[0].review_notes
 
+    session['redirect_recipe_id'] = recipe_id
     return render_template('recipe-details.html', recipe=recipe_details, show_fav_link=show_fav_link, ings=ingredients_list_image, n=n)
 
-    
-@app.route('/login')
-def login():
-    """View registration/login page."""
 
-    return render_template('login.html')
-
-
-@app.route('/signin', methods=["POST"])
+@app.route('/signin', methods=["GET", "POST"])
 def sign_in():
     """ Loging to the page """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    user = get_user(email)
-    # if password matches and is in db
-    if user and password == user.password:
-        session["user_id"] = user.user_id
-        session["fname"] = user.fname
-        flash("Logged in as %s" % user.fname)
-        if 'redirect_recipe_id' in session:
-            redirect_recipe_id = int(session['redirect_recipe_id'])
-            session['redirect_recipe_id'].clear()
-            if isinstance(redirect_recipe_id, int):
-                return redirect(f'/recipe/details?id={redirect_recipe_id}')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = get_user(email)
+        # if password matches and is in db
+        if user and password == user.password:
+            session["user_id"] = user.user_id
+            session["fname"] = user.fname
+            flash("Logged in as %s" % user.fname)
+            if 'redirect_recipe_id' in session:
+                redirect_recipe_id = int(session['redirect_recipe_id'])
+                session['redirect_recipe_id'].clear()
+                if isinstance(redirect_recipe_id, int):
+                    return redirect(f'/recipe/details?id={redirect_recipe_id}')
+            else:
+                return redirect('/')
+
+        # email is in db but typed wrong pwd
+        elif user and email == user.email:
+            flash("Please Check Your Password")
+            return redirect('/signin')
+
+        # if user not in db to signup
         else:
-            return redirect('/')
-
-    # email is in db but typed wrong pwd
-    elif user and email == user.email:
-        flash("Please Check Your Password")
-        return redirect('/login')
-
-    # if user not in db to signup
+            flash("PLEASE SIGNUP")
+            return redirect('/signup')
     else:
-        flash("PLEASE SIGNUP")
-        return redirect('/signup')
+        return render_template('login.html')
 
 
 @app.route('/logout')
@@ -194,10 +182,10 @@ def sign_up():
         user = get_user(email)
         if user:
             flash("Check your email and password")
-            return redirect('/login')
+            return redirect('/signin')
         else:
             create_user(firstname, lastname, email, password, mobileno)
-            return redirect('/login')
+            return redirect('/signin')
 
 
 @app.route('/my-fav')
@@ -238,105 +226,104 @@ def favorite():
         return render_template('my-favorites.html', fav_list=fav_list, my_recipe_list=my_recipe_list)
     else:
         flash("Login to see your favorites")
-        return redirect("/login")
+        return redirect("/signin")
 
-@app.route('/add-your-recipe')
-def add_your_recipe():
-    """ user can create his own recipe """
-
-    if 'user_id' in session:
-        user_id = session.get('user_id')
-        diet_picklist = get_diet()
-        dish_picklist = get_cuisine()
-        cuisine_picklist = get_dishtype()
-        ing_picklist = get_ingredients()
-        unit_picklist = get_unit()
-        return render_template('add-your-recipe.html', diet_picklist=diet_picklist, dish_picklist=dish_picklist, cuisine_picklist=cuisine_picklist, ing_picklist=ing_picklist, unit_picklist=unit_picklist)
-
-    else:
-        flash("Please login to create/add your recipe")
-        return redirect('/login')
-
-@app.route('/submit-your-recipe', methods=["POST"])
+@app.route('/submit-your-recipe', methods=["GET","POST"])
 def submit_your_recipe():
     """ submit the created recipe """
+    if request.method == 'POST':
     
-    if 'user_id' in session:
-        
-        user_id = session.get('user_id')
-        recipe_title = request.form.get("recipe-title")
-        recipe_instructions = request.form.get("instructions")
-        if request.form.get("preptime") == "":
-            prep_time = 0
-        else:
-            prep_time = request.form.get("preptime")
+        if 'user_id' in session:
 
-        if request.form.get("cooktime") == "":
-            cook_time = 0
-        else:
-            cook_time = request.form.get("cooktime")
+            
+            user_id = session.get('user_id')
+            recipe_title = request.form.get("recipe-title")
+            recipe_instructions = request.form.get("instructions")
+            if request.form.get("preptime") == "":
+                prep_time = 0
+            else:
+                prep_time = request.form.get("preptime")
 
-        recipe_description = request.form.get("description")
+            if request.form.get("cooktime") == "":
+                cook_time = 0
+            else:
+                cook_time = request.form.get("cooktime")
 
-        if request.form.get("diet") == "":
-            diet = 1
-        else:
-            diet = request.form.get("diet")
+            recipe_description = request.form.get("description")
 
-        if request.form.get("cuisine") == "":
-            cuisine = 1
-        else:
-            cuisine = request.form.get("cuisine")
+            if request.form.get("diet") == "":
+                diet = 1
+            else:
+                diet = request.form.get("diet")
 
-        if request.form.get("dishtype") == "":
-            dishtype = 1
-        else:
-            dishtype = request.form.get("dishtype")
-        url = ""
+            if request.form.get("cuisine") == "":
+                cuisine = 1
+            else:
+                cuisine = request.form.get("cuisine")
 
-        if request.form.get("ingrow") is None:
-            ingrow = 0
-            flash("enter at least one ingredient")
-        else:
-            ingrow = request.form.get("ingrow")
+            if request.form.get("dishtype") == "":
+                dishtype = 1
+            else:
+                dishtype = request.form.get("dishtype")
+            url = ""
 
-        ingredients = []
-        i = 1
+            if request.form.get("ingrow") is None:
+                ingrow = 0
+                flash("enter at least one ingredient")
+            else:
+                ingrow = request.form.get("ingrow")
 
-        while i <= int(ingrow):
-            ingredient_row = request.form.get(f"ingrow{i}")
-            # ingredient_tuple = tuple(map(str, ingredient_row.split(',')))
-            # new recipe ingredient rows from html[['1', '3', '3', 'papaya'], ['2', '3', '3'], ['1', '3', '3', 'test ingredient']]
-            # ['1', '3', '3', 'papaya']
-            # ingredient_list[0] = ingredient_id, ingredient_list[1] = unit,ingredient_list[2] = quantity, if other ingredient_list[3] = name,
-            print("######################### ingrow", ingredient_row)
-            if ingredient_row: 
-                ingredient_list = ingredient_row.split(',')
-                ingredients.append(ingredient_list)
-            i += 1
+            ingredients = []
+            i = 1
 
-        new_recipe = create_recipe(recipe_title, recipe_description, prep_time,
-                                   cook_time, dishtype, cuisine, diet, recipe_instructions, url, user_id)
-        new_recipe_id = new_recipe.recipe_id
-        if new_recipe_id:
-            for index, ingredient_list in enumerate(ingredients):
-                if ingredient_list[0] == "1":
-                    check_new_ing = get_ingredient_by_name(ingredient=ingredient_list[3])
+            while i <= int(ingrow):
+                ingredient_row = request.form.get(f"ingrow{i}")
+                # ingredient_tuple = tuple(map(str, ingredient_row.split(',')))
+                # new recipe ingredient rows from html[['1', '3', '3', 'papaya'], ['2', '3', '3'], ['1', '3', '3', 'test ingredient']]
+                # ['1', '3', '3', 'papaya']
+                # ingredient_list[0] = ingredient_id, ingredient_list[1] = unit,ingredient_list[2] = quantity, if other ingredient_list[3] = name,
+                print("######################### ingrow", ingredient_row)
+                if ingredient_row: 
+                    ingredient_list = ingredient_row.split(',')
+                    ingredients.append(ingredient_list)
+                i += 1
 
-                    if check_new_ing:
-                        ingredient_to_insert = check_new_ing.ingredient_id
+            new_recipe = create_recipe(recipe_title, recipe_description, prep_time,
+                                    cook_time, dishtype, cuisine, diet, recipe_instructions, url, user_id)
+            new_recipe_id = new_recipe.recipe_id
+            if new_recipe_id:
+                for index, ingredient_list in enumerate(ingredients):
+                    if ingredient_list[0] == "1":
+                        check_new_ing = get_ingredient_by_name(ingredient=ingredient_list[3])
+
+                        if check_new_ing:
+                            ingredient_to_insert = check_new_ing.ingredient_id
+                        else:
+                            new_ingredient = create_ingredient(
+                                ingredient_list[3], ingredient_list[1])
+                            ingredient_to_insert = new_ingredient.ingredient_id
+    
                     else:
-                        new_ingredient = create_ingredient(
-                            ingredient_list[3], ingredient_list[1])
-                        ingredient_to_insert = new_ingredient.ingredient_id
- 
-                else:
-                    ingredient_to_insert = int(ingredient_list[0])
+                        ingredient_to_insert = int(ingredient_list[0])
 
-                create_recipeingredient(
-                    ingredient_to_insert, new_recipe_id, ingredient_list[2])
+                    create_recipeingredient(
+                        ingredient_to_insert, new_recipe_id, ingredient_list[2])
 
-        return redirect('/my-fav')
+            return redirect('/my-fav')
+    else:
+        if 'user_id' in session:
+            user_id = session.get('user_id')
+            diet_picklist = get_diet()
+            dish_picklist = get_cuisine()
+            cuisine_picklist = get_dishtype()
+            ing_picklist = get_ingredients()
+            unit_picklist = get_unit()
+            return render_template('add-your-recipe.html', diet_picklist=diet_picklist, dish_picklist=dish_picklist, cuisine_picklist=cuisine_picklist, ing_picklist=ing_picklist, unit_picklist=unit_picklist)
+
+        else:
+            flash("Please login to create/add your recipe")
+            return redirect('/signin')
+
 
 
 @app.route('/add-to-favorite', methods=['POST'])
@@ -362,7 +349,7 @@ def add_to_your_favorite():
         return redirect(f'/recipe/details?id={recipe_id}')
     else:
         flash("Please Login before you add to your favorite")
-        return redirect("/login")
+        return redirect("/signin")
 
 
 if __name__ == '__main__':
